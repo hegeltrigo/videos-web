3455fb46ba990a6f27548d7f477562d50999cd8a
Hegel Trigo <hegeltrigo@gmail.com>
2018-11-19 23:45:59 -0400

    read data from api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout'
-import Main from './components/Main/Main/Main'
+import Layout from './components/Layout/Layout';
+import Main from './components/Main/Main/Main';
+import axios from 'axios';
+
 class App extends Component {
+
+  state = {
+    videos: []
+  }
+
+
+  componentDidMount() {
+    axios.get(`http://localhost:3001/api/v1/videos`)
+      .then(res => {
+        const videos = res.data;
+        console.log(videos)
+        this.setState({ videos });
+      })
+  }
+
   render() {
     return (
       <div>
          <Layout>
-           <Main/>
+           <Main videos={this.state.videos}/>
          </Layout>
       </div>
     );
--- src/components/Main/Main/Main.js
+++ src/components/Main/Main/Main.js
@@ -1,46 +1,14 @@
 import React from 'react'
 import classes from './Main.css'
 
-const main = () => (
+const main = (props) => (
   <div className={classes.Main}>
-    <div className={classes.video}>
-      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY"> </iframe>
+  
+    {props.videos.map((video,index) => {
+      return <div className={classes.video}>
+      <iframe width="100%" height="100%" src={'https://www.youtube.com/embed/' + video.youtube_video_id}> </iframe>
     </div>
-    <div className={classes.video}>
-      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY"> </iframe>
-    </div>
-    <div className={classes.video}>
-      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY"> </iframe>
-    </div>
-    <div className={classes.video}>
-      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY"> </iframe>
-    </div>
-
-    <div className={classes.video}>
-      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY"> </iframe>
-    </div>
-
-    <div className={classes.video}>
-      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY"> </iframe>
-    </div>
-
-    <div className={classes.video}>
-      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY"> </iframe>
-    </div>
-
-    <div className={classes.video}>
-      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY"> </iframe>
-    </div>
-    <div className={classes.video}>
-      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY"> </iframe>
-    </div>
-    <div className={classes.video}>
-      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY"> </iframe>
-    </div>
-    <div className={classes.video}>
-      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY"> </iframe>
-    </div>
-      
+    })} 
   </div>
 );
 
